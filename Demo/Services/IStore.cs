9791027cbb6e66c7cf18341339a0a99923c610e1
Stefan Jeglorz <S.Jeglorz@devware.de>
2024-07-11 20:12:41 +0200

    added demo project

---
+++ Demo/Services/IStore.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo;
+
+public interface IStore
+{
+    public event EventHandler<List<string>> OnCatalogChanged;
+
+    public List<string> Catalog { get; }
+    
+    public void AddStoreItem(string item);
+    public void RemoveStoreItem(string item);
+}
